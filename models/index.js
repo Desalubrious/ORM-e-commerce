5050cf68ca9992b6442624f45a7c55d877c6d314
Evan Mora <evanrmora@gmail.com>
2021-07-30 15:56:06 -0600

    Updated index.js

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// Import models
+const Product = require('./product');
+const Category = require('./category');
+const Tag = require('./tag');
+const ProductTag = require('./producttag');
+
+Product.belongsTo(Category);
+Category.hasMany(Product);
+Product.belongsToMany(Tag, { through: ProductTag ,foreignKey: 'tag_id'});
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id'});
